4c3ee3043a9cbfcc58080d362b5d48363c57dcdf
Will Myers <56778753+willmyethewebsiteguy@users.noreply.github.com>
2022-08-24 10:58:06 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@
 (function(){  
   const ps = {
     cssId: 'wm-mega-announcement',
-    cssFile: 'https://cdn.jsdelivr.net/gh/willmyethewebsiteguy/megaAnnouncementBar@1.0.003/styles.min.css'
+    cssFile: 'https://cdn.jsdelivr.net/gh/willmyethewebsiteguy/megaAnnouncementBar@1.0.004/styles.min.css'
   };
   const defaults = {
   };
